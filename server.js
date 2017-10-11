d900793718e4090ab1a2aba7181644e9e815b36f
Jack Cable <jackhcable@gmail.com>
2017-10-10 20:21:28 -0500

    Add access control headers

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var ObjectID = mongodb.ObjectID;
 var app = express();
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
